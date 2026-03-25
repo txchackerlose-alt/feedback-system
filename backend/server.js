909927a53573f776ceb0913bc9b2ab6092cdce96
priyanshu kumar <txchackerlose@gmail.com>
2026-03-26 02:31:25 +0530

    fix cors order+port

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+
 const authRoutes = require("./routes/authRoutes");
 
 app.use("/api/auth", authRoutes);
@@ -13,14 +16,12 @@ app.use("/api/feedback", feedbackRoutes);
 const concernRoutes = require("./routes/concernRoutes");
 
 app.use("/api/concerns", concernRoutes);
-app.use(cors());
-app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Supabase Backend Running 🚀");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
